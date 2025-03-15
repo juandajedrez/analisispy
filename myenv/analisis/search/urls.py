86a441dbc51d5100cbefcf0b21b3fbf4a3470c58
carlos <karlcorralesmovil@gmail.com>
2025-03-14 23:13:38 -0500

    create

--- myenv/analisis/search/urls.py
+++ myenv/analisis/search/urls.py
@@ -2,9 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path(
-        "",
-        views.form,
-    ),
-]
+urlpatterns = [path("/perfect", ]
--- myenv/analisis/search/views.py
+++ myenv/analisis/search/views.py
@@ -1,7 +1,23 @@
+import re
+
 from django.shortcuts import render
+from playwright.sync_api import Page, expect
 
 # Create your views here.
+import pytest
+from playwright.sync_api import Page, expect
 
+@pytest.fixture(scope="function", autouse=True)
+def before_each_after_each(page: Page):
+    
+    print("before the test runs")
 
-def form(request):
-    return render(request, "index.html")
+    # Go to the starting url before each test.
+    page.goto("https://playwright.dev/")
+    yield
+    
+    print("after the test runs")
+
+def test_main_navigation(page: Page):
+    # Assertions use the expect API.
+    expect(page).to_have_url("https://playwright.dev/")
